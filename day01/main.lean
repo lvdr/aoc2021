e67ea2a3b9fb6e6dc0ed9782b33e9682a064f221
lvdr <lav@lvdr.tech>
2021-12-04 12:10:47 +0200

    Day 01 cleanup

--- day01/main.lean
+++ day01/main.lean
@@ -9,20 +9,17 @@ def parse (α : String) : List Nat :=
     αₛ.map (stringAsNat 0)
 
 def increases : List Nat → Nat
-| []      => 0
-| [ω]     => 0
 | α::β::ω => (if β > α then 1 else 0) + increases (β::ω)
+| _       => 0
 
 def window : List Nat → List Nat
-| []     => []
-| [ω]    => []
-| [ω, ψ] => []
-| α::β::γ::ω      => (α + β + γ)::(window (β::γ::ω))
+| α::β::γ::ω => (α + β + γ)::(window (β::γ::ω))
+| _          => []
 
 def main (α : List String) : IO Unit := do
     let p := match α with
-    | []       => "input.in"
-    | (p₁::pₙ) => p₁
-    let ι <- IO.FS.readFile p
+    | []      => "day01/input.in"
+    | p₁::pₙ  => p₁
+    let ι ← IO.FS.readFile p
     IO.println (increases (parse ι))
     IO.println (increases (window (parse ι)))
