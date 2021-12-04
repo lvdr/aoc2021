cb2b32755bd45d441530e06bb3e16380b49c0cb1
lvdr <lav@lvdr.tech>
2021-12-04 16:13:20 +0200

    Day 02 cleanup

--- day02/main.lean
+++ day02/main.lean
@@ -1,41 +1,60 @@
-def stringAsInt (β : String) : Int :=
-    let βₙ := String.toInt? β 
+inductive Direction where
+  | forward
+  | down
+  | up
+  deriving Repr
+
+instance : ToString Direction where toString p :=
+    match p with
+    | Direction.forward => "forward"
+    | Direction.down    => "down"
+    | Direction.up      => "up"
+
+def stringAsNat (β : String) : Nat :=
+    let βₙ := String.toNat? β 
     match βₙ with
         | some γ => γ 
         | none   => 0
 
-def lineAsCoords (α : String) : Int × Int :=
+def lineAsCommands (α : String) : Direction × Nat :=
     let αₛ  := α.splitOn " "
     match αₛ with
     | [θ, Δ] =>
-      let Δ₁ := stringAsInt Δ
-      match θ with
-      | "forward" => (Δ₁, 0)
-      | "down"    => (0, Δ₁)
-      | "up"      => (0, -Δ₁)
-      | _         => (-1, -1)
-    | _ => (0, 0)
+      let Δ₁ := stringAsNat Δ
+      let θ₁ := match θ with
+      | "forward" => Direction.forward
+      | "down"    => Direction.down
+      | "up"      => Direction.up
+      | _         => Direction.forward
+      (θ₁, Δ₁)
+    | _ => (Direction.forward, 0)
 
-def parse (α : String) : List (Int × Int) :=
+def parse (α : String) : List (Direction × Nat) :=
     let αₛ := α.splitOn "\n"
-    αₛ.map lineAsCoords
+    αₛ.map lineAsCommands
 
 def add (α β : Int × Int) : Int × Int :=
-let (α₁, α₂) := α
-let (β₁, β₂) := β
-(α₁ + β₁, α₂ + β₂)
+    let (α₁, α₂) := α
+    let (β₁, β₂) := β
+    (α₁ + β₁, α₂ + β₂)
 
-def reduceAdd (α : List (Int × Int)) : Int × Int :=
-match α with
-| (Δx, Δy)::ω => add (Δx, Δy) (reduceAdd ω)
-| []          => (0, 0)
+def reduceAdd (α : List (Direction × Nat)) : Int × Int :=
+    match α with
+    | α₁::ω =>
+        match α₁ with
+        | (Direction.forward, Δx) => add (Δx, 0)  (reduceAdd ω)
+        | (Direction.down, Δy)    => add (0, Δy)  (reduceAdd ω)
+        | (Direction.up, Δy)      => add (0, -Δy) (reduceAdd ω)
+    | []                          => (0, 0)
 
-def reduceMove (α : Int) (β : List (Int × Int)) : Int × Int :=
-match β with
-| (Δx, 0)::ω => add (Δx, α * Δx) (reduceMove α ω)
-| (0, Δα)::ω => reduceMove (α + Δα) ω
-| (_, _)::ω   => (-1, -1)
-| _           => (0, 0)
+def reduceMove (α : Int) (β : List (Direction × Nat)) : Int × Int :=
+    match β with
+    | β₁::ω =>
+        match β₁ with
+        | (Direction.forward, Δx) => add (Δx, α * Δx) (reduceMove α ω)
+        | (Direction.down, Δα)    => reduceMove (α + Δα) ω
+        | (Direction.up, Δα)      => reduceMove (α - Δα) ω
+    | []                          => (0, 0)
 
 def main (α : List String) : IO Unit := do
     let p := match α with
